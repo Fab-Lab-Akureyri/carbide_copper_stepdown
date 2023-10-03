ab2794e9bcadfcb33c7bee266239a013e029ceb2
Árni Björnsson <arnib@vma.is>
2023-10-03 14:05:07 +0000

    Buxfix

--- copper_stepdown.py
+++ copper_stepdown.py
@@ -38,13 +38,25 @@ if g01_start_index is None:
     raise ValueError("G01 lines not found.")
 
 with open(output_file, 'w') as f:
-    # Write the lines before the original Z depth line
-    f.writelines(lines[:z_depth_line_index])
+    # Write the lines before the G90 command
+    g90_index = lines.index("G90\n")
+    f.writelines(lines[:g90_index + 1])
+    # Write the M03 command to start the spindle at 5000 rpm
+    f.write("M03 S5000.0\n")
+    # Write the lines after the G90 command up to the original Z depth line
+    f.writelines(lines[g90_index + 1:z_depth_line_index])
 
-    for multiplier in [1/3, 2/3, 1]:
+    # For all but the last pass, omit the M05 and M30 commands
+    for multiplier in [1/3, 2/3]:
         # Write the new Z depth
         f.write(f'G01 F200 Z{original_z_depth * multiplier:.3f}\n')
         # Write the G01 lines with X and Y coordinates
-        f.writelines(lines[g01_start_index:z_depth_line_index] + lines[z_depth_line_index + 1:])
+        for line in lines[g01_start_index:z_depth_line_index] + lines[z_depth_line_index + 1:]:
+            if line not in ["M05\n", "M30\n"]:
+                f.write(line)
+
+    # For the last pass, include everything
+    f.write(f'G01 F200 Z{original_z_depth:.3f}\n')
+    f.writelines(lines[g01_start_index:z_depth_line_index] + lines[z_depth_line_index + 1:])
 
 print(f'Modified file is written to {output_file}')
